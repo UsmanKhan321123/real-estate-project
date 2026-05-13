25d8d7f1b13b1859729da15ea7e82c0f1265ab57
UsmanKhan321123 <uk732161@gmail.com>
2026-05-13 13:39:01 +0500

    create user Schema using Prisma

--- backend/contollers/authControllers.js
+++ backend/contollers/authControllers.js
@@ -1,5 +1,22 @@
+import { hash, compare } from "bcrypt";
 
-export let login = (req,res) => {console.log(req.body);}
-export let signup = (req,res) => {console.log(req.body)}
-export let logout = (req,res) => {console.log(req.body)}
-
+export let signup = async (req, res) => {
+  let { username, email, password } = req.body;
+  if (!username || !email || !password) {
+    res.json({
+      messgae: "Please provide all fields",
+    });
+  }
+  let hashPassword = await hash(password, 10);
+  let newUSer = await prisma.user.create({
+    username,
+    email,
+    password: hashPassword,
+  });
+};
+export let login = (req, res) => {
+  console.log(req.body);
+};
+export let logout = (req, res) => {
+  console.log(req.body);
+};
